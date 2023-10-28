4e15fe4afa57b73b68ef2b44532f79340e706a7b
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-10-28 23:54:35 +0300

    Добавлены все обработчики проксирования

--- src/utils/create-deep-proxy.ts
+++ src/utils/create-deep-proxy.ts
@@ -2,24 +2,91 @@ declare type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 
+type GetOwnPropertyDescriptorHanderArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  key: string | symbol;
+};
+
+type OwnKeysHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+};
+
+type DefinePropertyHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  key: string | symbol;
+  descriptor: PropertyDescriptor;
+};
+
+type DeletePropertyHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  key: string | symbol;
+};
+
+type PreventExtensionsHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+};
+
+type HasHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  key: string | symbol;
+};
+
 type GetHandlerArgs<Target extends object> = {
   rootTarget: Target;
-  target: Record<string | symbol, any>;
   nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
   key: string | symbol;
+  reciever: any;
 };
 
 type SetHandlerArgs<Target extends object> = {
   rootTarget: Target;
-  target: Record<string | symbol, any>;
   nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
   key: string | symbol;
   value: any;
+  reciever: any;
+};
+
+type ApplyHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  thisArg: any;
+  args: any[];
+};
+
+type ConstructHandlerArgs<Target extends object> = {
+  rootTarget: Target;
+  nestedKeys: (string | symbol)[];
+  target: Record<string | symbol, any>;
+  args: any[];
+  newTarget: Function;
 };
 
 type DeepProxyHandler<Target extends object> = {
+  getOwnPropertyDescriptor?(args: GetOwnPropertyDescriptorHanderArgs<Target>): PropertyDescriptor | undefined;
+  ownKeys?(args: OwnKeysHandlerArgs<Target>): ArrayLike<string | symbol>;
+  defineProperty?(args: DefinePropertyHandlerArgs<Target>): boolean;
+  deleteProperty?(args: DeletePropertyHandlerArgs<Target>): boolean;
+  preventExtensions?(args: PreventExtensionsHandlerArgs<Target>): boolean;
+  has?(args: HasHandlerArgs<Target>): boolean;
   get?(args: GetHandlerArgs<Target>): any;
   set?(args: SetHandlerArgs<Target>): boolean;
+  apply?(args: ApplyHandlerArgs<Target>): any;
+  construct?(args: ConstructHandlerArgs<Target>): object;
 };
 
 export function createDeepProxy<Target extends object>(rootTarget: Target, handler: Prettify<DeepProxyHandler<Target>>) {
@@ -53,9 +120,18 @@ export function createDeepProxy<Target extends object>(rootTarget: Target, handl
 
     const proxyHandler: ProxyHandler<T> = {};
 
-    if (handler.get) proxyHandler.get = (target: T, key: string | symbol) => handler.get!({ rootTarget, target: target as any, nestedKeys: [...pathKeys, key], key });
-    if (handler.set)
-      proxyHandler.set = (target: T, key: string | symbol, value: any) => handler.set!({ rootTarget, target: target as any, nestedKeys: [...pathKeys, key], key, value });
+    if (handler.getOwnPropertyDescriptor)
+      proxyHandler.getOwnPropertyDescriptor = (target, key) => handler.getOwnPropertyDescriptor!({ rootTarget, nestedKeys: [...pathKeys, key], target, key });
+    if (handler.ownKeys) proxyHandler.ownKeys = (target) => handler.ownKeys!({ rootTarget, nestedKeys: pathKeys, target });
+    if (handler.defineProperty)
+      proxyHandler.defineProperty = (target, key, descriptor) => handler.defineProperty!({ rootTarget, nestedKeys: [...pathKeys, key], target, key, descriptor });
+    if (handler.deleteProperty) proxyHandler.deleteProperty = (target, key) => handler.deleteProperty!({ rootTarget, nestedKeys: [...pathKeys, key], target, key });
+    if (handler.preventExtensions) proxyHandler.preventExtensions = (target) => handler.preventExtensions!({ rootTarget, nestedKeys: pathKeys, target });
+    if (handler.has) proxyHandler.has = (target, key) => handler.has!({ rootTarget, nestedKeys: [...pathKeys, key], target, key });
+    if (handler.get) proxyHandler.get = (target, key, reciever) => handler.get!({ rootTarget, nestedKeys: [...pathKeys, key], target, key, reciever });
+    if (handler.set) proxyHandler.set = (target, key, value, reciever) => handler.set!({ rootTarget, nestedKeys: [...pathKeys, key], target: target as any, key, value, reciever });
+    if (handler.apply) proxyHandler.apply = (target, thisArg, args) => handler.apply!({ rootTarget, nestedKeys: pathKeys, target, thisArg, args });
+    if (handler.construct) proxyHandler.construct = (target, args, newTarget) => handler.construct!({ rootTarget, nestedKeys: pathKeys, target, args, newTarget });
 
     return new Proxy(target, proxyHandler);
   }
