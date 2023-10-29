73ebac4ff4efb063a49cb49673596cb970088e14
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-10-29 18:20:00 +0300

    Исправлен путь до стартового файла

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "./src/main.ts"),
+      entry: resolve(__dirname, "./index.ts"),
       name: "@novakod/deep-proxy",
     },
   },
