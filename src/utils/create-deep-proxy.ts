45e0bf1369a105ee15de2da38211d6d1ee0918df
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-10-31 19:43:37 +0300

    Добавлены все обработчики проксирования

--- src/utils/create-deep-proxy.ts
+++ src/utils/create-deep-proxy.ts
@@ -13,7 +13,7 @@ export function createDeepProxy<Target extends object>(rootTarget: Target, handl
       return proxiesMap.get(target);
     }
 
-    const proxy = new Proxy(target, {
+    const proxyHandler: ProxyHandler<Target> = {
       get(target, key, reciever) {
         const newPath = [...path, key];
         const value = handler.get ? handler.get({ target, key, path: newPath, reciever, rootTarget }) : Reflect.get(target, key, reciever);
@@ -34,7 +34,18 @@ export function createDeepProxy<Target extends object>(rootTarget: Target, handl
 
         return Reflect.deleteProperty(target, key);
       },
-    });
+    };
+
+    if (handler.getOwnPropertyDescriptor)
+      proxyHandler.getOwnPropertyDescriptor = (target, key) => handler.getOwnPropertyDescriptor!({ rootTarget, path: [...path, key], target, key });
+    if (handler.ownKeys) proxyHandler.ownKeys = (target) => handler.ownKeys!({ rootTarget, path, target });
+    if (handler.defineProperty) proxyHandler.defineProperty = (target, key, descriptor) => handler.defineProperty!({ rootTarget, path: [...path, key], target, key, descriptor });
+    if (handler.preventExtensions) proxyHandler.preventExtensions = (target) => handler.preventExtensions!({ rootTarget, path, target });
+    if (handler.has) proxyHandler.has = (target, key) => handler.has!({ rootTarget, path: [...path, key], target, key });
+    if (handler.apply) proxyHandler.apply = (target, thisArg, args) => handler.apply!({ rootTarget, path, target, thisArg, args });
+    if (handler.construct) proxyHandler.construct = (target, args, newTarget) => handler.construct!({ rootTarget, path, target, args, newTarget });
+
+    const proxy = new Proxy(target, proxyHandler);
 
     proxiesMap.set(target, proxy);
 
