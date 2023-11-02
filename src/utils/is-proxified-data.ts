cf4736e89d1076252cf7a62d250b9d55ed66339d
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-11-02 16:08:39 +0300

    Добавлено проксирование функций

--- src/utils/is-proxified-data.ts
+++ src/utils/is-proxified-data.ts
@@ -1,3 +1,3 @@
 export function isProxifiedData(data: any): data is object {
-  return typeof data === "object" && data !== null;
+  return (typeof data === "object" && data !== null) || typeof data === "function";
 }
