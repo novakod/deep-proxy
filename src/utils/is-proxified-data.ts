c5d4f064103b54fee469cfefbee1885bef7268cc
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2024-08-03 18:00:45 +0300

    Теперь прототип объекта не проксируется

--- src/utils/is-proxified-data.ts
+++ src/utils/is-proxified-data.ts
@@ -1,3 +1,5 @@
 export function isProxifiedData(data: any): data is object {
+  if (data === Object.prototype) return false;
+
   return (typeof data === "object" && data !== null) || typeof data === "function";
 }
